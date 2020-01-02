247edd7f74740f79cb1fd383107cbd7e767f83b4
13664505637 <13664505637@163.com>
2020-01-02 17:11:43 +0800

    Initial commit

---
+++ src/test/java/com/yuyang/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuyang.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
